e599760b29daa8c1ebc7b97dc43c821cf73791f2
Octavio Palacios <86976885+octaviopalacioscodanai@users.noreply.github.com>
2021-10-29 17:24:18 -0600

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const dbConfig = {
   host: 'localhost',
   port: 3306,
-  user: 'root',
-  password: 'admin123',
+  user: '',
+  password: '',
   database: 'PingPong',
 };
 
